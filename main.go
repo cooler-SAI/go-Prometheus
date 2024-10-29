5ae70054a1a65b12d7e84319a4cc2de80b4effe2
cooler-SAI <cooler19860212@yahoo.com>
2024-10-29 23:12:25 +0300

    added base prometheus app

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/http"
 	"os"
+)
+
+var (
+	httpRequests = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "HTTP requests count",
+	})
 )
 
 func main() {
 
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 
-	log.Info().Msg("Hello go-Prometheus")
+	log.Info().Msg("go-Prometheus app starting...")
+
+	prometheus.MustRegister(httpRequests)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		httpRequests.Inc()
+		log.Info().Msg("Main Page request added!")
+		_, err := w.Write([]byte("Hello, Prometheus with Zerolog!"))
+		if err != nil {
+			return
+		}
+	})
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	log.Info().Msg(" Started Server on port 8080!")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal().Err(err).Msg("Error start server!")
+	}
+
 }
